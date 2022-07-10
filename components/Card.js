6a12f741f85a4565b81bbdf0ce06a221710198b1
Arjun Khanduri <arjun99.khanduri@gmail.com>
2022-07-10 16:36:17 +0530

    Adding Cards

--- components/Card.js
+++ components/Card.js
@@ -1,12 +1,33 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Image, Text } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
-const Card = () => {
-  return (
-    <View>
-      <Text>Card</Text>
-    </View>
-  )
+import { COLORS, SIZES, SHADOWS, assets } from '../constants';
+
+const Card = ({ data }) => {
+    return (
+        <View style={{
+            backgroundColor: COLORS.white,
+            borderRadius: SIZES.font,
+            marginBottom: SIZES.extraLarge,
+            margin: SIZES.base,
+            ...SHADOWS.dark
+        }}>
+            <View style={{ width: '100%', height: 250 }}>
+                <Image
+                    source={data.image}
+                    resizeMode="cover"
+                    style={{
+                        width: "100%",
+                        height: "100%",
+                        borderTopLeftRadius: SIZES.font,
+                        borderTopRightRadius: SIZES.font
+                    }}
+                />
+                
+            </View>
+            <Text>Card</Text>
+        </View>
+    )
 }
 
 export default Card
--- constants/dummy.js
+++ constants/dummy.js
@@ -59,6 +59,64 @@ const Data = [
       },
     ],
   },
+  {
+    id: "PHOTO-03",
+    name: "My photo 2",
+    creator: "Original John Doe",
+    price: 400,
+    description:
+      "Average photo",
+    image: assets.photo3,
+    bids: [
+      {
+        id: "REVIEW-01",
+        name: "John Doe 1",
+        image: assets.person02,
+        date: "Review 1 date",
+      },
+      {
+        id: "REVIEW-02",
+        name: "John Doe 2",
+        image: assets.person03,
+        date: "Review 2 date",
+      },
+      {
+        id: "REVIEW-03",
+        name: "John Doe 3",
+        image: assets.person04,
+        date: "Review 3 date",
+      },
+    ],
+  },
+  {
+    id: "PHOTO-04",
+    name: "My photo 2",
+    creator: "Original John Doe",
+    price: 400,
+    description:
+      "Average photo",
+    image: assets.photo4,
+    bids: [
+      {
+        id: "REVIEW-01",
+        name: "John Doe 1",
+        image: assets.person02,
+        date: "Review 1 date",
+      },
+      {
+        id: "REVIEW-02",
+        name: "John Doe 2",
+        image: assets.person03,
+        date: "Review 2 date",
+      },
+      {
+        id: "REVIEW-03",
+        name: "John Doe 3",
+        image: assets.person04,
+        date: "Review 3 date",
+      },
+    ],
+  },
 ];
 
 export { Data };
