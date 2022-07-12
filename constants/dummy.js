59088df138286f028dcd8a66fc8e50b1c438b86e
Arjun Khanduri <arjun99.khanduri@gmail.com>
2022-07-12 10:30:28 +0530

    Adding dummy comments

--- constants/dummy.js
+++ constants/dummy.js
@@ -15,18 +15,21 @@ const Data = [
         name: "John Doe 1",
         image: assets.person02,
         date: "Review 1 date",
+        comment: "I like it"
       },
       {
         id: "REVIEW-02",
         name: "John Doe 2",
         image: assets.person03,
         date: "Review 2 date",
+        comment: "I like it"
       },
       {
         id: "REVIEW-03",
         name: "John Doe 3",
         image: assets.person04,
         date: "Review 3 date",
+        comment: "I like it"
       },
     ],
   },
@@ -44,18 +47,21 @@ const Data = [
         name: "John Doe 1",
         image: assets.person02,
         date: "Review 1 date",
+        comment: "I like it"
       },
       {
         id: "REVIEW-02",
         name: "John Doe 2",
         image: assets.person03,
         date: "Review 2 date",
+        comment: "I like it"
       },
       {
         id: "REVIEW-03",
         name: "John Doe 3",
         image: assets.person04,
         date: "Review 3 date",
+        comment: "I like it"
       },
     ],
   },
@@ -73,18 +79,21 @@ const Data = [
         name: "John Doe 1",
         image: assets.person02,
         date: "Review 1 date",
+        comment: "I like it"
       },
       {
         id: "REVIEW-02",
         name: "John Doe 2",
         image: assets.person03,
         date: "Review 2 date",
+        comment: "I like it"
       },
       {
         id: "REVIEW-03",
         name: "John Doe 3",
         image: assets.person04,
         date: "Review 3 date",
+        comment: "I like it"
       },
     ],
   },
@@ -102,18 +111,21 @@ const Data = [
         name: "John Doe 1",
         image: assets.person02,
         date: "Review 1 date",
+        comment: "I like it"
       },
       {
         id: "REVIEW-02",
         name: "John Doe 2",
         image: assets.person03,
         date: "Review 2 date",
+        comment: "I like it"
       },
       {
         id: "REVIEW-03",
         name: "John Doe 3",
         image: assets.person04,
         date: "Review 3 date",
+        comment: "I like it"
       },
     ],
   },
